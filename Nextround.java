a835f8784c34de00cd78f5829d08f2e7111571fb
rajibul07 <74898533+rajibul07@users.noreply.github.com>
2021-11-01 12:33:09 +0530

    Update Nextround.java

--- Nextround.java
+++ Nextround.java
@@ -1,5 +1,12 @@
+/*
+"Contestant who earns a score equal to or greater than the k-th place finisher's score will advance to the next round, as long as the contestant earns a positive score..."
+— an excerpt from contest rules.
+
+A total of n participants took part in the contest (n ≥ k), and you already know their scores. Calculate how many participants will advance to the next round.
+*/
+
+
 import java.util.*;
-//import java.util.*;
 public class solution{
     public static void main(String[] args){
     Scanner sc=new Scanner(System.in);
