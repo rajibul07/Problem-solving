2cf944854ae9174433404a68aa8f592a3cab43e1
rajibul07 <74898533+rajibul07@users.noreply.github.com>
2021-11-01 12:54:51 +0530

    Create BoyorGirl.java

---
+++ BoyorGirl.java
@@ -0,0 +1,35 @@
+/*
+Those days, many boys use beautiful girls' photos as avatars in forums. So it is pretty hard to tell the gender of a user at the first glance. Last year, our hero went to a
+forum and had a nice chat with a beauty (he thought so).
+After that they talked very often and eventually they became a couple in the network.
+
+But yesterday, he came to see "her" in the real world and found out "she" is actually a very strong man! Our hero is very sad and he is too tired to love again now. 
+So he came up with a way to recognize users' genders by their user names.
+
+This is his method: if the number of distinct characters in one's user name is odd, then he is a male, otherwise she is a female. You are given the string that denotes the
+user name, please help our hero to determine the gender of this user by his method.
+*/
+
+import java.util.*;
+public class solution{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+ 
+        String str=sc.next();
+        int n=str.length();
+        int cnt=0;
+        char arr[]=str.toCharArray();
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]==arr[i+1])
+             cnt++;
+        }
+        int s=n-cnt;
+        if(s%2==0){
+        System.out.println("CHAT WITH HER!");
+        }
+        else{
+            System.out.println("IGNORE HIM!");
+        }
+    }
+}
