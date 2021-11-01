cabe34ba9dda6415b2f4201d4a2de3af591a0e55
rajibul07 <74898533+rajibul07@users.noreply.github.com>
2021-11-01 12:35:44 +0530

    Add files via upload

---
+++ beautyMatrix.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.Math;
+
+class solution{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+
+        int x=0,ans=0;
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                x=sc.nextInt();
+
+                if(x==1){
+                    ans+=Math.abs(i-2);
+                    ans+=Math.abs(j-2);
+                }
+            }
+        }
+        System.out.println(ans);
+
+    }
+}
