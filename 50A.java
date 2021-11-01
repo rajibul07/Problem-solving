495dbb8ef14a38e6798a4bb23160e98074592259
rajibul07 <74898533+rajibul07@users.noreply.github.com>
2021-11-01 12:26:52 +0530

    Add files via upload

---
+++ 50A.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+ class solution{
+    public static void main(String[] args){
+        
+        Scanner sc=new Scanner(System.in);
+        int M=sc.nextInt();
+        int N=sc.nextInt();
+        
+        System.out.println((int)(Math.ceil(M*N/2)));
+    }
+}
