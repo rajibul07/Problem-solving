61262164eb204eef2dbc8ba3ded556151351eee4
rajibul07 <74898533+rajibul07@users.noreply.github.com>
2021-11-01 12:27:42 +0530

    Update 50A.java

--- 50A.java
+++ 50A.java
@@ -1,5 +1,6 @@
 /*
-You are given a rectangular board of M × N squares. Also you are given an unlimited number of standard domino pieces of 2 × 1 squares. You are allowed to rotate the pieces. You are asked to place as many dominoes as possible on the board so as to meet the following conditions:
+You are given a rectangular board of M × N squares. Also you are given an unlimited number of standard domino pieces of 2 × 1 squares. You are allowed to rotate the pieces.
+You are asked to place as many dominoes as possible on the board so as to meet the following conditions:
 
 1. Each domino completely covers two squares.
 
